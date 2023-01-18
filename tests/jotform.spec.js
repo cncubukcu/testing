09dced12ab4f70dd3a2a59e816e855b89c2efbc5
Can Cubukcu <can@jotform.com>
2023-01-18 14:54:55 +0300

    feat: init
    feat: init

---
+++ tests/jotform.spec.js
@@ -0,0 +1,28 @@
+/* 
+  We want you to test the Jotform's login page.
+  First test case:
+  - Go to https://jotform.com/login/
+  - Check wheather the social login buttons are exist.
+  - Check Email and Password fields are exist.
+  - Check the login button is exist.
+*
+
+
+/*
+  Second test case:
+  - Fill the email and password fields with correct credentials.
+  - Click the login button.
+  - Check the user is logged in.
+*/
+
+/* 
+  Third test case:
+  - test the login page with invalid credentials.
+*/
+
+/*
+  Fourth test case:
+  - test Forget password button is working and go back to login page.
+*/
+
+const { test, expect } = require('@playwright/test');
